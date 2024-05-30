f9c2c052370af45f89e19e5c21b8e7801179b18e
wmoooid <threeqletters@yandex.ru>
2024-05-30 18:23:31 +0400

    shadcn init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,22 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import { cn } from '@/shared/lib/utils';
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-main'
+});
 
 export const metadata: Metadata = {
     title: 'Reddit',
     description: 'Reddit client FSD example'
 };
 
-export default function RootLayout({
-    children
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>{children}</body>
         </html>
     );
 }
